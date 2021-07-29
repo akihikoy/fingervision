d197ca11afa3feefca14faea1bbadf38c2a46767
Akihiko Yamaguchi <info@akihikoy.net>
2021-07-29 20:30:35 +0900

    Modified the camera configuration tool to print configuration in YAML and base64 formats;
    Fixed a bug;

--- fingervision/scripts/conf_cam_gui.py
+++ fingervision/scripts/conf_cam_gui.py
@@ -11,29 +11,32 @@ import roslib
 roslib.load_manifest('ay_py')
 from ay_py.core import CPrint
 from ay_py.tool.py_panel import TSimplePanel, InitPanelApp, RunPanelApp, AskYesNoDialog, QtCore, QtGui
-import sys
+import os,sys
 import subprocess
 import re
+import yaml
+import base64
+import zlib
 
 def ExecCmd(cmd):
   p= subprocess.Popen(cmd, stdout=subprocess.PIPE)
   (stdout, stderr)= p.communicate()
   exit_code= p.wait()
   return stdout.strip() if stdout is not None else stdout, stderr.strip() if stderr is not None else stderr, exit_code
-'''
-  Exposure, Auto
-    ID      : 0x0000000f,
-    Type    : Choice,
-    Flags   : { CAN_READ, CAN_WRITE },
-    Values  : { 'Auto Mode'[0], 'Manual Mode'[1], 'Shutter Priority Mode'[2], 'Aperture Priority Mode'[3] },
-    Default : 3
-  Exposure (Absolute)
-    ID      : 0x00000011,
-    Type    : Dword,
-    Flags   : { CAN_READ, CAN_WRITE },
-    Values  : [ 1 .. 1331, step size: 1 ],
-    Default : 120
-'''
+
+def EncodeDictB64(d, with_compress=True):
+  d_yaml= yaml.dump(d)
+  if with_compress:  d_yaml_c= zlib.compress(d_yaml)
+  else:  d_yaml_c= d_yaml
+  d_b64= base64.b64encode(d_yaml_c)
+  return d_b64
+
+def DecodeDictB64(d_b64, with_compress=True):
+  d_yaml_c= base64.b64decode(d_b64)
+  if with_compress:  d_yaml= zlib.decompress(d_yaml_c)
+  else:  d_yaml= d_yaml_c
+  d= yaml.load(d_yaml)
+  return d
 
 def GetControls(cam_dev):
   stdout,stderr,ec= ExecCmd(['uvcdynctrl', '-d', cam_dev, '-c'])
@@ -59,10 +62,13 @@ def GetControls(cam_dev):
     details['Flags']= re.findall(r'[a-zA-Z0-9_]+', details['Flags'])
     details['Default']= int(details['Default'])
     if details['Type']=='Choice':
+      #print 'debug',ctrl,details['Values']
       values= re.findall(r'\'[a-zA-Z0-9\s]+\'\[[0-9]+\]', details['Values'])
       matches= [re.match(r'\'([a-zA-Z0-9\s]+)\'\[([0-9]+)\]', value) for value in values]
+      #print '----',values,'/',matches
       values= {int(match.group(2)): match.group(1) for match in matches}
-      details['Values']= [values[i] if i in values else None for i in range(sorted(values.keys())[-1])]
+      #print '----',values
+      details['Values']= [values[i] if i in values else None for i in range(sorted(values.keys())[-1]+1)]
     elif details['Type'] in ('Boolean', 'Dword'):
       #(min,max,step) tuple.
       details['Values']= map(int, re.match(r'\[\s*([\-\+0-9]+)\s*\.\.\s*([\-\+0-9]+)\s*,\s*step size\:\s*([\-\+0-9]+)\s*\]', details['Values']).groups())
@@ -72,19 +78,27 @@ def GetCtrlValue(cam_dev, ctrl):
   stdout,stderr,ec= ExecCmd(['uvcdynctrl', '-d', cam_dev, '-g', ctrl])
   return int(stdout.strip())
 
+def GetCtrlValues(cam_dev, ctrls):
+  return {ctrl: GetCtrlValue(cam_dev, ctrl) for ctrl in ctrls}
+
 def SetCtrlValue(cam_dev, ctrl, value):
   #NOTE: '--' is neccessary for nevative values (not neccessary for positive ones).
   stdout,stderr,ec= ExecCmd(['uvcdynctrl', '-d', cam_dev, '-s', ctrl, '--', str(value)])
 
+def SetCtrlValues(cam_dev, ctrl_values):
+  for ctrl,value in ctrl_values.iteritems():
+    SetCtrlValue(cam_dev, ctrl, value)
+
 
 def GenerateImgCtrlWidges(cam_dev):
   ctrls,ctrl_details= GetControls(cam_dev)
-  print 'ctrls:',ctrls
-  print 'ctrl_details:',ctrl_details
+  #print 'ctrls:',ctrls
+  #print 'ctrl_details:',ctrl_details
 
   ctrl_values= {ctrl: GetCtrlValue(cam_dev, ctrl) for ctrl in ctrls}
   for ctrl in ctrls:
     value= ctrl_values[ctrl]
+    #print 'debug:',ctrl,value,ctrl_details[ctrl]['Values']
     print ctrl,'=',value,ctrl_details[ctrl]['Values'][value] if ctrl_details[ctrl]['Type']=='Choice' else ''
 
   def onvaluechange(ctrl, value):
@@ -134,6 +148,12 @@ def GenerateImgCtrlWidges(cam_dev):
 
 if __name__=='__main__':
   cam_dev= sys.argv[1] if len(sys.argv)>1 else '/dev/video0'
+  if not os.path.exists(cam_dev):
+    raise Exception('Device not found:',cam_dev)
+
+  def Print(*s):
+    for ss in s:  print ss,
+    print ''
 
   widges_img_ctrl,layout_img_ctrl,ctrls,ctrl_details= GenerateImgCtrlWidges(cam_dev)
 
@@ -142,6 +162,16 @@ if __name__=='__main__':
       'label',{
         'text': 'Device: '+cam_dev,
         'size_policy': ('expanding', 'minimum')}),
+    'btn_yaml': (
+      'button',{
+        'text': 'YAML',
+        'size_policy': ('expanding', 'fixed'),
+        'onclick': lambda w,obj: Print(yaml.dump(GetCtrlValues(cam_dev,ctrls))), }),
+    'btn_b64': (
+      'button',{
+        'text': 'Base64',
+        'size_policy': ('expanding', 'fixed'),
+        'onclick': lambda w,obj: Print(EncodeDictB64(GetCtrlValues(cam_dev,ctrls))), }),
     'btn_exit': (
       'button',{
         'text': 'Exit',
@@ -151,7 +181,7 @@ if __name__=='__main__':
 
   layout_main= (
     'boxv',None,(
-      ('boxh',None, ('label_device','btn_exit')),
+      ('boxh',None, ('label_device','btn_yaml','btn_b64','btn_exit')),
       layout_img_ctrl,
       ))
 
