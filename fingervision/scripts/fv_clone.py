7738493b3e772d567ced9ae2f842cdc6ff7e8aab
Akihiko Yamaguchi <info@akihikoy.net>
2025-04-15 05:55:44 +0900

    Updated the cloning script of FV node (improved the service clones);

--- fingervision/scripts/fv_clone.py
+++ fingervision/scripts/fv_clone.py
@@ -25,41 +25,49 @@ if __name__=='__main__':
   fv_from= rospy.get_param('~fv_from', fv_from)
   fv_to= rospy.get_param('~fv_to', fv_to)
 
-  pub_bm= rospy.Publisher(rospy.get_namespace()+'{fv}/blob_moves'.format(fv=fv_to),
-                          fingervision_msgs.msg.BlobMoves, queue_size=10)
-  pub_pv= rospy.Publisher(rospy.get_namespace()+'{fv}/prox_vision'.format(fv=fv_to),
-                          fingervision_msgs.msg.ProxVision, queue_size=10)
+  pub,sub,srvp,srv= {},{},{},{}
 
-  sub_bm= rospy.Subscriber(rospy.get_namespace()+'{fv}/blob_moves'.format(fv=fv_from),
-                           fingervision_msgs.msg.BlobMoves, lambda msg:pub_bm.publish(ModifyFV(msg,fv_to)))
-  sub_pv= rospy.Subscriber(rospy.get_namespace()+'{fv}/prox_vision'.format(fv=fv_from),
-                           fingervision_msgs.msg.ProxVision, lambda msg:pub_pv.publish(ModifyFV(msg,fv_to)))
+  #Clone a publisher of fv_from for fv_to.
+  def clone_pub(topic, msg_type, queue_size=10):
+    pub[topic]= rospy.Publisher(rospy.get_namespace()+'{fv}/{topic}'.format(fv=fv_to, topic=topic),
+                                msg_type, queue_size=queue_size)
+    sub[topic]= rospy.Subscriber(rospy.get_namespace()+'{fv}/{topic}'.format(fv=fv_from, topic=topic),
+                                 msg_type, lambda msg:pub[topic].publish(ModifyFV(msg,fv_to)))
 
+  clone_pub('blob_moves',  fingervision_msgs.msg.BlobMoves)
+  clone_pub('prox_vision', fingervision_msgs.msg.ProxVision)
 
-  srvp_pause= rospy.ServiceProxy('/fingervision/{fv}/pause'.format(fv=fv_from),
-                                     std_srvs.srv.Empty)
-  srvp_resume= rospy.ServiceProxy('/fingervision/{fv}/resume'.format(fv=fv_from),
-                                     std_srvs.srv.Empty)
-  srvp_clear_obj= rospy.ServiceProxy('/fingervision/{fv}/clear_obj'.format(fv=fv_from),
-                                     std_srvs.srv.Empty)
-  srvp_set_frame_skip= rospy.ServiceProxy('/fingervision/{fv}/set_frame_skip'.format(fv=fv_from),
-                                          fingervision_msgs.srv.SetInt32)
-  srvp_start_detect_obj= rospy.ServiceProxy('/fingervision/{fv}/start_detect_obj'.format(fv=fv_from),
-                                            std_srvs.srv.Empty)
-  srvp_stop_detect_obj= rospy.ServiceProxy('/fingervision/{fv}/stop_detect_obj'.format(fv=fv_from),
-                                            std_srvs.srv.Empty)
+  #Clone a service of fv_from for fv_to.
+  #  dummy: If True, a similar service is created for fv_to but the functionality is removed.
+  def clone_srv(service, srv_type, dummy=False):
+    srvp[service]= rospy.ServiceProxy('/fingervision/{fv}/{service}'.format(fv=fv_from, service=service),
+                                      srv_type)
+    if not dummy:
+      srv[service]= rospy.Service('/fingervision/{fv}/{service}'.format(fv=fv_to, service=service),
+                                  srv_type, lambda req:srvp[service](req))
+    else:
+      srv[service]= rospy.Service('/fingervision/{fv}/{service}'.format(fv=fv_to, service=service),
+                                  srv_type, lambda req:srv_type._response_class())
 
-  srv_pause= rospy.Service('/fingervision/{fv}/pause'.format(fv=fv_to),
-                           std_srvs.srv.Empty, lambda req:srvp_pause(req))
-  srv_resume= rospy.Service('/fingervision/{fv}/resume'.format(fv=fv_to),
-                            std_srvs.srv.Empty, lambda req:srvp_resume(req))
-  srv_clear_obj= rospy.Service('/fingervision/{fv}/clear_obj'.format(fv=fv_to),
-                               std_srvs.srv.Empty, lambda req:srvp_clear_obj(req))
-  srv_set_frame_skip= rospy.Service('/fingervision/{fv}/set_frame_skip'.format(fv=fv_to),
-                                    fingervision_msgs.srv.SetInt32, lambda req:srvp_set_frame_skip(req))
-  srv_start_detect_obj= rospy.Service('/fingervision/{fv}/start_detect_obj'.format(fv=fv_to),
-                                      std_srvs.srv.Empty, lambda req:srvp_start_detect_obj(req))
-  srv_stop_detect_obj= rospy.Service('/fingervision/{fv}/stop_detect_obj'.format(fv=fv_to),
-                                     std_srvs.srv.Empty, lambda req:srvp_stop_detect_obj(req))
+  clone_srv('pause'            , std_srvs.srv.Empty, dummy=True)
+  clone_srv('resume'           , std_srvs.srv.Empty, dummy=True)
+  clone_srv('show_windows'     , std_srvs.srv.Empty, dummy=True)
+  clone_srv('hide_windows'     , std_srvs.srv.Empty, dummy=True)
+  clone_srv('start_record'     , std_srvs.srv.Empty, dummy=True)
+  clone_srv('stop_record'      , std_srvs.srv.Empty, dummy=True)
+  clone_srv('set_video_prefix' , fingervision_msgs.srv.SetString, dummy=True)
+  clone_srv('set_frame_skip'   , fingervision_msgs.srv.SetInt32, dummy=True)
+  clone_srv('take_snapshot'    , fingervision_msgs.srv.TakeSnapshot, dummy=True)
+  clone_srv('stop_detect_obj'  , std_srvs.srv.Empty, dummy=True)
+  clone_srv('start_detect_obj' , std_srvs.srv.Empty, dummy=True)
+  clone_srv('clear_obj'        , std_srvs.srv.Empty, dummy=True)
+  clone_srv('req_calibrate'     , fingervision_msgs.srv.SetStringInt32, dummy=True)
+  clone_srv('req_initialize'    , fingervision_msgs.srv.SetStringInt32, dummy=True)
+  clone_srv('set_dim_level'     , fingervision_msgs.srv.SetStringInt32, dummy=True)
+  clone_srv('set_trackbar_mode' , fingervision_msgs.srv.SetStringInt32, dummy=True)
+  clone_srv('save_parameters'   , fingervision_msgs.srv.SetString, dummy=True)
+  clone_srv('load_parameters'   , fingervision_msgs.srv.SetString, dummy=True)
+  clone_srv('save_calibration'  , fingervision_msgs.srv.SetStringInt32, dummy=True)
+  clone_srv('load_calibration'  , fingervision_msgs.srv.SetStringInt32, dummy=True)
 
   rospy.spin()
